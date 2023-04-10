2cf92f08b2a0ea78ac2b8e5d76abf2fe739b6ce6
dothedro <drooooofficial@gmail.com>
2023-04-11 00:39:51 +0800

    added: first step learning

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      title: 'Sample Title',
+      sampleDigit: 1
+    }
+  },
+  methods: {
+    handleClick() {
+      this.sampleDigit++
+    }
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>{{ title }}</h1>
+    <button @click="handleClick">{{ sampleDigit }} Clicks</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <h1>Hello</h1>
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
